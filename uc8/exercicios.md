2959ac1495968852943289f6f5cc04478006aefa
CARLOS ALBERTO MAIELLO JUNIOR <carlos.amjunior2@senacsp.edu.br>
2022-06-14 16:30:21 -0300

    Funções de Agregação

--- uc8/exercicios.md
+++ uc8/exercicios.md
@@ -1,5 +1,7 @@
 # Exercícios
 
+## WHERE e INNER JOIN
+
 1. Mostre todos os cursos da área 1
 2. Selecione todos cursos onde:
    1. os nomes iniciam a letra 'A'
@@ -17,3 +19,16 @@
 8. Mostre quais são os alunos matriculados na turma do técnico em informática
 9. Mostre quais são os alunos cursando a área de Saúde
 10. Mostre em que turma, curso e área cada aluno está matriculado
+
+## Funções de Agregação
+11. Mostre a quantidade de turmas cadastradas
+12. Mostre a quantidade de alunos cadastrados
+13. Mostre a quantidade de alunos matriculados
+14. Mostre o preço médio dos cursos da escola
+15. Mostre uma lista de turmas e a quantidade de alunos por turma
+16. Mostre uma lista de turmas e o valor total de receita de cada turma 
+17. Mostre uma lista com os cursos e o valor total de receita de cada curso
+18. Mostre uma lista com as áreas e quantidade de cursos por área
+19. Mostre uma lista com todos os alunos e em quantidas turmas eles estão matriculados
+20. Mostre uma lista com as áreas e quantos alunos existem por área
+21. Mostre uma lista com as áreas e os cursos e quantos alunos existem por curso
--- uc8/select.sql
+++ uc8/select.sql
@@ -1,3 +1,5 @@
+/* WHERE E INNER JOIN */
+
 SELECT * FROM areas;
 
 SELECT * FROM areas WHERE nome like 'A%';
@@ -34,3 +36,28 @@ select cursos.id as 'Código', cursos.nome as 'Curso', areas.nome as 'Área', cu
 select cursos.id as 'Código', cursos.nome as 'Curso', areas.nome as 'Área', cursos.preco as 'Preço'  from areas inner join cursos on (cursos.area_id = areas.id) where cursos.preco > 100;
 
 select cursos.id as 'Código', cursos.nome as 'Curso', areas.nome as 'Área', cursos.preco as 'Preço'  from areas inner join cursos on (cursos.area_id = areas.id) where cursos.preco < 300;
+
+
+/* FUNÇÕES DE AGREGAÇÃO */
+
+select count(*) as qtde from alunos;
+
+select count(*), max(preco), min(preco), avg(preco), sum(preco) from cursos;
+
+
+select areas.nome, count(*),max(preco), min(preco), avg(preco) 
+from areas 
+  inner join cursos on (areas.id = cursos.area_id) 
+group by areas.nome;
+
+select areas.nome, count(cursos.id),max(preco), min(preco), avg(preco) 
+from areas 
+  left join cursos on (areas.id = cursos.area_id) 
+group by areas.nome;
+
+
+select areas.nome, count(*), max(preco), min(preco), avg(preco) 
+from areas 
+  inner join cursos on (areas.id = cursos.area_id) 
+group by areas.nome
+having avg(preco) > 200;
