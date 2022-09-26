f94103f4fa59632bc75d974fbae301756d6fda5c
Carlos Maiello <carlosmaiello@gmail.com>
2022-09-26 14:51:40 -0300

    objetos

---
+++ uc13/objetos.js
@@ -0,0 +1,25 @@
+
+var boletim = {
+    aluno: "José",
+    n1: 8,
+    n2: 10,
+    n3: 9,
+    n4: 7,
+    media: 8.5 
+};
+
+console.log(boletim.media);
+
+console.log(boletim);
+
+var notas = [];
+notas.push(boletim);
+
+notas.push({
+    aluno: "Maria",
+    n1: 8,
+    n2: 10,
+    n3: 9,
+    n4: 7,
+    media: 8.5
+})
