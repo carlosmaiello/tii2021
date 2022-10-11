19bad1117b4f97d3daf41e65bb2ca1a3568997e9
Carlos Maiello <carlosmaiello@gmail.com>
2022-10-11 16:35:18 -0300

    NewsDetail

--- uc13/News/src/pages/NewsPage.js
+++ uc13/News/src/pages/NewsPage.js
@@ -1,9 +1,24 @@
-import { Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import React, { useEffect } from "react";
+import { Image, Text, View } from "react-native";
+import NewsItem from "../components/NewsItem";
+import { NewsContext } from "../contexts/News";
 
 export default function NewsPage() {
+    const { newsList, listNews } = React.useContext(NewsContext);
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        listNews();
+    }, []);
+
     return (
         <View>
-            <Text>NewsPage</Text>
+            {newsList.map(news => (<NewsItem news={news} onPress={() => {
+                navigation.navigate("NewsDetail", {
+                    newsId: news.id
+                })
+            }}/>))}
         </View>
     )
 }
