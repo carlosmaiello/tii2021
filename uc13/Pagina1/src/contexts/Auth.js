cde120b85ef45113b83fecb652711fd05f5203ec
Carlos Maiello <carlosmaiello@gmail.com>
2022-10-17 16:49:03 -0300

    Gravar usuário logado

--- uc13/Pagina1/src/contexts/Auth.js
+++ uc13/Pagina1/src/contexts/Auth.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const AuthContext = React.createContext();
 
@@ -7,20 +8,66 @@ export const AuthProvider = ({ children }) => {
 
     const [user, setUser] = React.useState(null);
 
-    const login = (email, password) => {
+    const login = async (email, password) => {
         if (email == "a@a.com" && password == "123") {
-            setUser({
+            const user = {
                 email: "a@a.com",
                 name: "Joselito"
-            })
+            };
+            setUser(user);
+            try {
+                await AsyncStorage.setItem('user', JSON.stringify(user));
+            }
+            catch (err) {
+                console.log(err);
+            }
         }
         else
             setUser(null);
     }
 
-    const logout = () => {
+    const logout = async () => {
         setUser(null);
+        try {
+            await AsyncStorage.setItem('user', null);
+        }
+        catch (err) {
+            console.log(err);
+        }
     }
+
+    const carregar = async () => {
+        try {
+            const v = await AsyncStorage.getItem('user');
+            setUser(v != null ? JSON.parse(v) : null);
+        }
+        catch (err) {
+            setUser(null);
+            console.log(err);
+        }
+    }
+
+    // Quando monta o componente
+    useEffect(() => {
+        carregar();
+        console.log("MONTAR");
+
+        return () => {
+            // Aqui executa quando desmonta
+            console.log("DESMONTAR");
+        }
+    }, []);
+
+    // Quando atualiza o componente
+    useEffect(() => {
+        console.log("ATUALIZAR");
+    });
+
+    useEffect(() => {
+        console.log("USUÁRIO ATUALIZADO");
+    }, [user]);
+
+
 
     const authorized = user != null;
 
